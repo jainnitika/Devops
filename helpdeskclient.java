7f2b232250fa597e12415e22fe56940b0bda0200
jainnitika <2019hw86813@wilp.bits-pilani.ac.in>
2022-06-16 22:59:13 +0530

    feature2 added

--- helpdeskclient.java
+++ helpdeskclient.java
@@ -1,1 +1,1 @@
- welcome to help desk client - feature 1 
+ welcome to help desk client - feature 1 and feature 2
--- helpdeskserver.java
+++ helpdeskserver.java
@@ -1,1 +1,1 @@
- welcome to help desk server - feature 1 
+ welcome to help desk server - feature 1  and feature 2
